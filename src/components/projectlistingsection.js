a64f5db1e565acdbd9b435e25ee7fd9d4a5423c5
Keen Ballon <keenballon@gmail.com>
2020-08-24 11:48:29 +0800

    another file path change

--- src/components/projectlistingsection.js
+++ src/components/projectlistingsection.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import ProjectListing from "./ProjectListing"
+import ProjectListing from "./projectlisting"
 
 const StyledListingSection = styled.section`
   margin-bottom: 2em;
