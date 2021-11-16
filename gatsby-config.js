c683caa76a41051dd06736afad1f32ab88c7210d
Keen <keenballon@gmail.com>
2021-11-16 22:35:14 +0800

    removed packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,6 @@ module.exports = {
             },
          },
       },
-      `gatsby-plugin-preload-fonts`,
       "gatsby-plugin-react-helmet",
       {
          resolve: `gatsby-source-filesystem`,
